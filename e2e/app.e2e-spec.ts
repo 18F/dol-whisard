9fa67993311ebbff078f3719487981d2e17a111a
Brendan Sudol <brendan.sudol@gsa.gov>
2017-08-07 11:48:30 -0400

    init

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { DolWhisardPage } from './app.po';
+
+describe('dol-whisard App', () => {
+  let page: DolWhisardPage;
+
+  beforeEach(() => {
+    page = new DolWhisardPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
---
+++ e2e/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class DolWhisardPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('app-root h1')).getText();
+  }
+}
