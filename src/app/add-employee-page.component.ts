b9f509d8ab7a681a3ff60ed78a10c41a8ce77b75
Brendan Sudol <brendan.sudol@gsa.gov>
2017-08-28 09:56:56 -0400

    add site navigation, start add employee page

---
+++ src/app/add-employee-page.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-add-employee',
+  templateUrl: './add-employee-page.component.html',
+})
+export class AddEmployeePageComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { RouterModule, Routes } from '@angular/router'
 
+import { AddEmployeePageComponent } from './add-employee-page.component'
 import { AppComponent } from './app.component'
 import { DataFieldComponent } from './data-field.component'
 import { ImportPageComponent } from './import-page.component'
@@ -11,12 +12,14 @@ import { FileReaderService } from './_service/file-reader.service'
 
 const routes: Routes = [
   { path: '', component: ImportPageComponent },
+  { path: 'add-employee', component: AddEmployeePageComponent },
   { path: '**', component: NotFoundComponent },
 ]
 
 @NgModule({
   imports: [BrowserModule, RouterModule.forRoot(routes)],
   declarations: [
+    AddEmployeePageComponent,
     AppComponent,
     ImportPageComponent,
     NotFoundComponent,
